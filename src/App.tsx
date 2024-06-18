61259a9d6265eaa05f8f5adbaf96d1dde8d353d9
Owen Kleinhans <92853486+owenkl21@users.noreply.github.com>
2024-06-18 23:10:07 +0200

    Started working with a backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,52 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import Expandable from './components/Expandable';
-import { useState } from 'react';
-import Form from './components/Form';
-import ExpenseList from './ExpenseTracker/ExpenseList';
-import Filter from './ExpenseTracker/Filter';
-import ExpenseForm from './ExpenseTracker/ExpenseForm';
-import categories from './ExpenseTracker/categies';
+import axios, { CanceledError } from 'axios';
+import { useEffect, useState } from 'react';
+import { DiVim } from 'react-icons/di';
+
+interface User {
+  id: number;
+  name: string;
+}
 
 function App() {
-  const [selectedCategory, setSelectedCategory] = useState('');
-  const [expenses, setExpenses] = useState([
-    {
-      id: 1,
-      description: 'Milk',
-      amount: 100,
-      category: 'Groceries',
-    },
-    {
-      id: 2,
-      description: 'Coffee',
-      amount: 120,
-      category: 'Groceries',
-    },
-    {
-      id: 3,
-      description: 'Sugar',
-      amount: 140,
-      category: 'Groceries',
-    },
-  ]);
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
 
-  const handleDelete = (id: number) => {
-    const newExpenses = expenses.filter((expense) => expense.id !== id);
-    setExpenses(newExpenses);
-  };
+  useEffect(() => {
+    const controller = new AbortController();
+    setIsLoading(true);
+    axios
+      .get<User[]>('https://jsonplaceholder.typicode.com/users', {
+        signal: controller.signal,
+      })
+      .then((res) => {
+        setUsers(res.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setIsLoading(false);
+      });
 
-  const visibleExpenses = selectedCategory
-    ? expenses.filter((e) => e.category === selectedCategory)
-    : expenses;
+    return () => controller.abort();
+  }, []);
 
   return (
     <>
-      <ExpenseForm
-        onSubmit={(expense) =>
-          setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
-        }
-      />
-      <Filter
-        onSelectCategory={(category) => {
-          setSelectedCategory(category);
-        }}
-      />
-      <ExpenseList expenses={visibleExpenses} onDelete={handleDelete} />
+      {error ? (
+        <div className="alert alert-danger" role="alert">
+          {error}
+        </div>
+      ) : isLoading ? (
+        <div className="spinner-border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div>
+      ) : (
+        users.map((user) => <div key={user.id}>{user.name}</div>)
+      )}
     </>
   );
 }
